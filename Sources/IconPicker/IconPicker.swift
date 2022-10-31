23019672513a5fef37f05f96cbb9dd9c393fb452
em_iot@wp.pl <em_iot@wp.pl>
2022-10-31 19:36:23 +0100

    all but one parameters optional

--- Sources/IconPicker/IconPicker.swift
+++ Sources/IconPicker/IconPicker.swift
@@ -36,10 +36,10 @@ public struct IconPickerView: View {
     
     public init(iconTapAction: @escaping (_ iconName: String) -> Void, searchFieldTitle: String? = nil, headerView: AnyView? = nil, triggerSizeAndCoordinates: CGRect? = nil, connectorColor: Color? = nil) {
         self.iconTapAction = iconTapAction
-        self.searchFieldTitle = searchFieldTitle ?? nil
-        self.headerView = headerView ?? nil
-        self.triggerSizeAndCoordinates = triggerSizeAndCoordinates ?? nil
-        self.connectorColor = connectorColor ?? nil
+        self.searchFieldTitle = searchFieldTitle ?? "Search"
+        self.headerView = headerView ?? AnyView(_fromValue: VStack{})
+        self.triggerSizeAndCoordinates = triggerSizeAndCoordinates ?? CGRect(x: 0, y: 0, width: 100.0, height: 20.0)
+        self.connectorColor = connectorColor ?? Color.orange
     }
     
     
