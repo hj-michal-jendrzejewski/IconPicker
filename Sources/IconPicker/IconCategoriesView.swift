58e6b4ffd53983e5631d546693a7fd086b5d0166
em_iot@wp.pl <em_iot@wp.pl>
2022-11-30 17:27:28 +0100

    another debugger

--- Sources/IconPicker/IconCategoriesView.swift
+++ Sources/IconPicker/IconCategoriesView.swift
@@ -91,6 +91,7 @@ struct IconCategoriesView: View {
     func setVisibleCategoryNameAsTitle(offsetKey: ViewOffsetKey.Value) -> Void {
         if self.iconsCategoriesOffsetsReverese[Double(offsetKey)].count > 0 {
             print(self.iconsCategoriesOffsetsReverese[Double(Int(offsetKey))][0])
+            print(Double(Int(offsetKey)))
             let visibleCategoryName = self.iconsCategoriesOffsetsReverese[Double(Int(offsetKey))][0].replacingOccurrences(of: "AAPinned", with: "Pinned").replacingOccurrences(of: "AHistory", with: "History").replacingOccurrences(of: "ASuggestions", with: "Suggestions")
             self.visibleCategoryName = visibleCategoryName
         }
